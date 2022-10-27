818dd7c555091a43083a022ecce098dd17092b96
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2022-10-27 16:27:32 +0900

    Update 사칙연산.py

--- lv0/사칙연산.py
+++ lv0/사칙연산.py
@@ -7,3 +7,11 @@ def solution(num1, num2):
     return num1 + num2
 
 # 두 수의 차
+# def solution(num1, num2):
+#     answer = num1 - num2
+#     return answer
+
+def solution(num1, num2):
+    return num1 - num2
+
+# 두 수의 곱
