62b48a6c51b459ce3ed14555889b7554152a2975
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2022-10-28 15:56:10 +0900

    Update Day1.py
    사칙연산

--- lv0/Day1.py
+++ lv0/Day1.py
@@ -1,31 +1,31 @@
 # 두 수의 합
 # def solution(num1, num2):
-#   answer = num1 + num2
-#   return answer
+#     answer = num1 + num2
+#     return answer
     
 def solution(num1, num2):
-  return num1 + num2
+    return num1 + num2
 
 # 두 수의 차
 # def solution(num1, num2):
-#   answer = num1 - num2
-#   return answer
+#     answer = num1 - num2
+#     return answer
 
 def solution(num1, num2):
-  return num1 - num2
+    return num1 - num2
 
 # 두 수의 곱
 # def solution(num1, num2):
-#   answer = num1 * num2
-#   return answer
+#     answer = num1 * num2
+#     return answer
 
 def solution(num1, num2):
-  return num1 * num2
+    return num1 * num2
 
 # 몫 구하기
 # def solution(num1, num2):
-#   answer = num1 // num2
-#   return answer
+#     answer = num1 // num2
+#     return answer
 
 def solution(num1, num2):
-  return num1 // num2
+    return num1 // num2
