d1f71508c54fbcf27b78451a8584ef54ae8ee7eb
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2022-10-28 15:54:43 +0900

    Update 사칙연산.py

--- lv0/사칙연산.py
+++ lv0/사칙연산.py
@@ -1,39 +1,31 @@
 # 두 수의 합
 # def solution(num1, num2):
-#     answer = num1 + num2
-#     return answer
+#   answer = num1 + num2
+#   return answer
     
 def solution(num1, num2):
-    return num1 + num2
+  return num1 + num2
 
 # 두 수의 차
 # def solution(num1, num2):
-#     answer = num1 - num2
-#     return answer
+#   answer = num1 - num2
+#   return answer
 
 def solution(num1, num2):
-    return num1 - num2
+  return num1 - num2
 
 # 두 수의 곱
 # def solution(num1, num2):
-#     answer = num1 * num2
-#     return answer
+#   answer = num1 * num2
+#   return answer
 
 def solution(num1, num2):
-    return num1 * num2
-
-# 두 수의 나눗셈
-# def solution(num1, num2):
-#     answer = int((num1 / num2) * 1000)
-#     return answer
-
-def solution(num1, num2):
-    return int((num1 / num2) * 1000)
+  return num1 * num2
 
 # 몫 구하기
 # def solution(num1, num2):
-#     answer = num1 // num2
-#     return answer
+#   answer = num1 // num2
+#   return answer
 
 def solution(num1, num2):
-    return num1 // num2
+  return num1 // num2
