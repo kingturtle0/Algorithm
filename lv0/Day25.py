72cc9c5e6261555cff55aef9e77f34741436e266
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2022-11-26 21:50:51 +0900

    Create Day25.py

---
+++ lv0/Day25.py
@@ -0,0 +1,11 @@
+# 문자열 밀기
+# def solution(A, B):
+#     answer = 0
+#     while A!=B:
+#         A = A[-1]+A[:-1]
+#         answer += 1
+#         if answer>=len(A):
+#             return -1
+#     return answer
+
+solution=lambda a,b:(b*2).find(a)
