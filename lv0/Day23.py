024f066d9f304b474ac88f3a09794f16b2745614
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2022-11-29 21:08:12 +0900

    Create Day23.py

---
+++ lv0/Day23.py
@@ -0,0 +1,24 @@
+# 특이한 정렬
+
+# 등수 매기기
+def solution(score):
+    answer = [sum(i)/2 for i in score]
+    return [sorted(answer, reverse=True).index(i)+1 for i in answer]
+
+# 옹알이 (1)
+
+
+# 로그인 성공?
+# def solution(id_pw, db):
+#     if id_pw in db:
+#         return "login"
+#     else:
+#         if id_pw[0] in [i[0] for i in db]:
+#             return "wrong pw"
+#         else:
+#             return "fail"
+
+def solution(id_pw, db):
+    if db_pw := dict(db).get(id_pw[0]):
+        return "login" if db_pw == id_pw[1] else "wrong pw"
+    return "fail"
