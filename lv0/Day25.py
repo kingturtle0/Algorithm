fbc8ae51cca7d8f7399f3e2eb95c53074dbacf39
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2022-11-30 19:45:08 +0900

    Update Day25.py

--- lv0/Day25.py
+++ lv0/Day25.py
@@ -13,3 +13,28 @@ solution=lambda a,b:(b*2).find(a)
 # 종이 자르기
 def solution(M, N):
     return M*N-1
+
+# 연속된 수의 합
+# def solution(num, total):
+#     if num%2==0:
+#         answer = list(range(int(total/num)-int(num/2)+1,int(total/num)+int(num/2)+1))
+#     else:
+#         answer = list(range(int(total/num)-int(num/2),int(total/num)+int(num/2)+1))
+#     return answer
+
+def solution(num, total):
+    answer = []
+    var = sum(range(num+1))
+    diff = total - var
+    start_num = diff//num
+    answer = [i+1+start_num for i in range(num)]
+    return answer
+
+# 다음에 올 숫자
+def solution(common):
+    answer = 0
+    if common[1]-common[0]==common[2]-common[1]:
+        answer = common[-1]+common[1]-common[0]
+    else:
+        answer = common[-1]*(common[1]/common[0])
+    return answer
