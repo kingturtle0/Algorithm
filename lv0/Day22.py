6a0a443013795f10db2b3059b758bbe30781f25f
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2022-12-01 13:54:19 +0900

    Update Day22.py

--- lv0/Day22.py
+++ lv0/Day22.py
@@ -1,5 +1,15 @@
 # 저주의 숫자 3
+# def solution(n):
+#     answer = [i for i in range(1,200) if i%3!=0 and "3" not in str(i)]
+#     return answer[n-1]
 
+def solution(n):
+    answer = 0
+    for _ in range(n):
+        answer += 1
+        while answer % 3 == 0 or '3' in str(answer):
+            answer += 1
+    return answer
 
 # 평행
 
