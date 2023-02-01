ee9b3ae7df5b5ea9dab580c94054b635d034a7b4
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2023-02-01 23:06:07 +0900

    Update 1859. 백만 장자 프로젝트.py

--- SWEA/D2/1859. 백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트.py
@@ -13,4 +13,23 @@ for test_case in range(1, T + 1):
 
     print(f"#{test_case} {profit}")
     
-# 런타임 에러
+# 런타임 에러(2중 for문)
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    length = int(input())
+    prices = list(map(int, input().split()))
+
+    cnt = 0
+    profit = 0
+    for i in range(length-1, 0, -1):
+        if prices[i+cnt] > prices[i-1]:
+            profit += (prices[i+cnt] - prices[i-1])
+            cnt += 1
+        else:
+            cnt = 0
+
+    print(f'#{test_case} {profit}')
+    
+# 1중 for문으로 해결
