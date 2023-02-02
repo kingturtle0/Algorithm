b94ec11555378da398b5d60ac64998a7a27c9c2c
kingturtle0 <115979198+kingturtle0@users.noreply.github.com>
2023-02-02 12:07:04 +0900

    Update 1945. 간단한 소인수분해.py

--- SWEA/D2/1945. 간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해.py
@@ -20,3 +20,19 @@ for test_case in range(1, T + 1):
             num //= 11
             e += 1
     print(f'#{test_case}', a, b, c, d, e)
+# 위는 하나씩 다 쓴 코드
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    degree = [0] * 5
+    numbers = [2, 3, 5, 7, 11]
+    while N != 1:
+        for i in range(5):
+            if N % numbers[i] == 0:
+                N //= numbers[i]
+                degree[i] += 1
+
+    a, b, c, d, e = degree
+    print(f'#{test_case} {a} {b} {c} {d} {e}')
