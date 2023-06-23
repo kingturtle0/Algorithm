a4e02a7772f2311d233351d4d423b1b96bc65f15
kingturtle0 <dhdnqkrtk22@naver.com>
2023-06-23 23:11:17 +0900

    Update BOJ solved question 06232023

---
+++ BAEKJOON/S5/11651. 좌표 정렬하기 2.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+arr.sort(key=lambda x:[x[1], x[0]])
+for lst in arr:
+    print(*lst)
