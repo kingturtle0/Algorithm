2f9088a507c32e423abbd8d913e64b1e48ad57fe
kingturtle0 <dhdnqkrtk22@naver.com>
2023-09-07 21:42:25 +0900

    Update BOJ solved question 09072023

---
+++ BAEKJOON/B2/1100. 하얀 칸.py
@@ -0,0 +1,12 @@
+chess = [list(input()) for _ in range(8)]
+ans = 0
+for i in range(8):
+    for j in range(8):
+        if i % 2 == 0:
+            if j % 2 == 0 and chess[i][j] == 'F':
+                ans += 1
+        else:
+            if j % 2 == 1 and chess[i][j] == 'F':
+                ans += 1
+
+print(ans)
