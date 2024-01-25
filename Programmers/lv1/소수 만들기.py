5d09d3afc399db0dea522962c876bd6b5897837c
오영재 <dhdnqkrtk22@naver.com>
2024-01-25 23:24:25 +0900

    Update Programmers solved question 01252024

---
+++ Programmers/lv1/소수 만들기.py
@@ -0,0 +1,16 @@
+def solution(nums):
+    answer = 0
+    primes = [0, 0] + [1] * 2999
+    for i in range(4, 3001):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                primes[i] = 0
+    
+    n = len(nums)
+    for i in range(n - 2):
+        for j in range(i + 1, n - 1):
+            for k in range(j + 1, n):
+                if primes[nums[i] + nums[j] + nums[k]]:
+                    answer += 1
+    
+    return answer
